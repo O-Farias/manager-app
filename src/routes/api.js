09f3b27b2f5cfa6d336dc425a8384dc56dce8d7b
Mateus Farias <mateus.f2001@hotmail.com>
2024-08-04 15:30:45 -0300

    Adicionado Conteroller para empréstimos

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const booksController = require("../controllers/booksController");
-
+const booksController = require("../Controllers/books-controller");
+const loansController = require("../Controllers/loans-controller");
+const { ensureAuth } = require("../Middlewares/auth-middleware");
 const apiRouter = express.Router();
 
 apiRouter.get("/books", booksController.index);
 apiRouter.get("/books/:id", booksController.show);
+
 apiRouter.post("/books", booksController.save);
 apiRouter.put("/books/:id", booksController.update);
 apiRouter.delete("/books/:id", booksController.delete);
 
-module.exports = apiRou;
+apiRouter.get("/loans", loansController.index);
+apiRouter.get("/loans/:id", loansController.show);
+apiRouter.post("/loans", ensureAuth, loansController.save);
+apiRouter.post("/loans/:id/return", loansController.return);
+
+module.exports = apiRouter;
